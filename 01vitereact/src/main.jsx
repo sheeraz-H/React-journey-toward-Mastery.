fa82e25966db617f13e917ec60711bb69d506cb6
sheeraz-H <xherazusain@gmail.com>
2025-10-27 11:17:51 -0700

    upgraded components

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Hello Abhijeeth!</h1>
+      <h3>Hmm, bool ma daya bol raha hun</h3>
+
+      <h1>ha daya teri bhen ki codh</h1>
+    </div>
+  )
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>,
 )

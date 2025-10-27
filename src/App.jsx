559e030cd190e24858cf386b1ce06df112b72227
sheeraz-H <your-github-xherazusain@gmail.com>
2025-10-27 03:13:06 -0700

    components errors handling

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Pros from "./pros"
+
+function App() {
+
+  return (
+    <>
+    <Pros/>
+    <h1>hello jamiat</h1>
+    <p>mile linderBergh</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pros.jsx
@@ -0,0 +1,7 @@
+function Pros(){
+  return(
+    <h3>Hey, I'm a pro 'architect'.</h3>
+  )
+}
+
+export default Pros
